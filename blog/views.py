ebf397040ac230fd04d5e1839e4a26bdc1979454
Danny <superhotfire447@yandex.ru>
2020-03-30 15:51:29 +0300

    show popular posts on main

--- blog/views.py
+++ blog/views.py
@@ -27,9 +27,15 @@ def serialize_tag(tag):
     }
 
 
-def index(request):
+def get_likes_count(post):
+    return post.likes.count()
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+
+def index(request):
+    posts = Post.objects.all()
+
+    # TODO. Как это посчитать?
+    most_popular_posts = sorted(posts, key=lambda post: get_likes_count(post))[-5:-1]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
